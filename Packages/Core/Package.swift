3d55b816ee432f304270e89eaca9fdf3cb94473a
Daichi Hayashi <develop@daichidaiji.com>
2024-06-21 18:58:37 +0900

    Add swift-format

--- Packages/Core/Package.swift
+++ Packages/Core/Package.swift
@@ -4,11 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "Core",
-    products: [
-        .library(name: "Core", targets: ["Core"]),
-    ],
-    targets: [
-        .target(name: "Core"),
-    ]
+  name: "Core",
+  products: [
+    .library(name: "Core", targets: ["Core"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-format", from: "510.1.0"),
+  ],
+  targets: [
+    .target(name: "Core"),
+  ]
 )
