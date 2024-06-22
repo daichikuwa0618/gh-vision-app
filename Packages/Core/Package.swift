66ed44ad63b4039d7712f5c9663586dd8f37e563
Daichi Hayashi <develop@daichidaiji.com>
2024-06-22 22:40:26 +0900

    Add swift-dependencies

--- Packages/Core/Package.swift
+++ Packages/Core/Package.swift
@@ -3,15 +3,30 @@
 
 import PackageDescription
 
+extension Target.Dependency {
+  static var dependencies: Self { .product(name: "Dependencies", package: "swift-dependencies") }
+  static var dependenciesMacros: Self { .product(name: "DependenciesMacros", package: "swift-dependencies") }
+}
+
 let package = Package(
   name: "Core",
+  platforms: [
+    .iOS(.v15),
+  ],
   products: [
     .library(name: "Core", targets: ["Core"]),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-format", from: "510.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.3.1"),
   ],
   targets: [
-    .target(name: "Core"),
+    .target(
+      name: "Core",
+      dependencies: [
+        .dependencies,
+        .dependenciesMacros,
+      ]
+    ),
   ]
 )
