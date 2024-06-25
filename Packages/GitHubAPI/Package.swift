835d2af7a9e18b59dbbe8627cd0506ac50e438a8
Daichi Hayashi <develop@daichidaiji.com>
2024-06-25 17:38:18 +0900

    Add OpenAPI Generator

--- Packages/GitHubAPI/Package.swift
+++ Packages/GitHubAPI/Package.swift
@@ -10,10 +10,18 @@ let package = Package(
     ],
     dependencies: [
       .package(url: "https://github.com/apple/swift-format", from: "510.1.0"),
+      .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
+      .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
+      .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
     ],
     targets: [
         .target(
-            name: "GitHubAPI"
+            name: "GitHubAPI",
+            dependencies: [
+              .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+              .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+            ],
+            plugins: [.plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")]
         ),
     ]
 )
