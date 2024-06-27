3246809a9a875c2d599c179785ffbd443beac589
Daichi Hayashi <develop@daichidaiji.com>
2024-06-27 12:10:06 +0900

    Add Xcode project

---
+++ GHVisionApp/GHVisionApp/GHVisionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHVisionAppApp.swift
+//  GHVisionApp
+//
+//  Created by Daichi Hayashi on 2024/06/27.
+//
+
+import SwiftUI
+
+@main
+struct GHVisionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
